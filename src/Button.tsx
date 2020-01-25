ecc46571fea8733bd0ab112dcf926dc621e1faf9
kazuma1989 <funifuni.1204@gmail.com>
2020-01-25 21:04:07 +0900

    feat: オープニング画面を追加

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,11 +1,13 @@
 import css from 'https://unpkg.com/csz'
 
 export default function Button({
+  theme,
   label,
   disabled,
   onClick,
   class: className = '',
 }: {
+  theme?: 'default' | 'primary' | 'danger'
   label?: string
   disabled?: boolean
   onClick?(): void
@@ -13,9 +15,9 @@ export default function Button({
 }) {
   return (
     <button
+      class={`${style} ${theme} ${className}`}
       type="button"
       disabled={disabled}
-      class={`${button} ${className}`}
       onClick={onClick}
     >
       {label}
@@ -23,11 +25,24 @@ export default function Button({
   )
 }
 
-const button = css`
+const style = css`
   border-radius: 2vw;
   padding: 2vw 5vw;
-
   background-color: var(--gray50);
 
   user-select: none;
+
+  &:disabled {
+    filter: opacity(0.5);
+  }
+
+  &.primary {
+    background-color: var(--cyanBlue10);
+    color: var(--white);
+  }
+
+  &.danger {
+    background-color: var(--red10);
+    color: var(--white);
+  }
 `
--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,13 @@ import { render } from '/web_modules/preact.js'
 import { useState } from '/web_modules/preact/hooks.js'
 import css from 'https://unpkg.com/csz'
 import Button from './Button.js'
+import OpeningView from './OpeningView.js'
 
 function SomePreactComponent() {
   const [active, setActive] = useState(false)
   const toggle = () => setActive(v => !v)
 
-  return (
-    <h1
-      class={css`
-        color: ${active ? 'var(--red)' : 'inherit'};
-      `}
-    >
-      Hello, World!
-      <Button label="Toggle" onClick={toggle} />
-    </h1>
-  )
+  return <OpeningView />
 }
 
 render(<SomePreactComponent />, document.getElementById('root')!)
