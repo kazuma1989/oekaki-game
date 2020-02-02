197c39f33c808bac468d0af44b32d7d9e6ee071d
kazuma1989 <funifuni.1204@gmail.com>
2020-02-02 15:27:35 +0900

    refactor: Workbox Window を導入

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,29 @@
-import { render } from '/web_modules/preact.js'
+import { Workbox } from 'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-window.prod.mjs'
+import * as preact from '/web_modules/preact.js'
 import { createStore } from '/web_modules/redux.js'
 import { reducer, Provider } from './reducer.js'
 import App from './App.js'
 
+self.React = preact
+
 const store = createStore(
   reducer,
   undefined,
-  (window as any)?.__REDUX_DEVTOOLS_EXTENSION__?.(),
+  (self as any)?.__REDUX_DEVTOOLS_EXTENSION__?.(),
 )
 
-render(
+preact.render(
   <Provider value={store}>
     <App />
   </Provider>,
   document.getElementById('root')!,
 )
+
+if ('serviceWorker' in navigator) {
+  const wb = new Workbox('/sw.js')
+
+  wb.register()
+}
 
 if (!['127.0.0.1', 'localhost'].includes(location.hostname)) {
   // https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,6 +3,10 @@
 /// <reference types="preact"/>
 import React = preact
 
+declare module 'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-window.prod.mjs' {
+  export * from 'workbox-window'
+}
+
 declare module 'https://unpkg.com/csz' {
   /**
    * @example
