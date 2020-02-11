b57101a300eb4d137a208d01d8c76a1eb68c66c4
kazuma1989 <funifuni.1204@gmail.com>
2020-02-11 11:53:53 +0900

    refactor: 離脱時にアラートを表示しない
    データが常に保存されるため、不要になった

--- src/index.tsx
+++ src/index.tsx
@@ -45,16 +45,3 @@ if ('serviceWorker' in navigator) {
     wb.register()
   })
 }
-
-if (!['127.0.0.1', 'localhost'].includes(location.hostname)) {
-  // https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event
-  window.addEventListener('beforeunload', event => {
-    console.log('beforeunload')
-
-    event.preventDefault()
-    // Chrome requires returnValue to be set.
-    // が、とくに設定した文字が表示されるわけでもない
-    event.returnValue =
-      'このサイトを離れてもよろしいですか？行った変更が保存されない可能性があります。'
-  })
-}
