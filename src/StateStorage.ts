f52ed22372b359840fd9742172f876959d44fc91
kazuma1989 <funifuni.1204@gmail.com>
2020-02-14 09:02:36 +0900

    refactor: 最低限の改ざん防止
    無邪気に書き換えるのを防ぐ程度

--- src/StateStorage.ts
+++ src/StateStorage.ts
@@ -2,24 +2,53 @@ export default class StateStorage<T> {
   constructor(
     readonly storage: Storage = localStorage,
     readonly key = `${StateStorage.name}.root-state`,
+    readonly integrityKey = `${StateStorage.name}.integrity`,
   ) {}
 
   get(): T | undefined {
     try {
-      return JSON.parse(this.storage.getItem(this.key) ?? 'null')
+      const value = this.storage.getItem(this.key)
+      if (!value) return
+
+      const integrity = this.storage.getItem(this.integrityKey)
+      if (integrity !== integrityOf(value)) {
+        console.debug(`${this.key}: wrong integrity`)
+        return
+      }
+
+      return JSON.parse(value)
     } catch (e) {
       console.warn(e)
-      this.storage.removeItem(this.key)
 
-      return undefined
+      this.storage.removeItem(this.key)
+      this.storage.removeItem(this.integrityKey)
     }
   }
 
   set(state: T): void {
     try {
-      this.storage.setItem(this.key, JSON.stringify(state))
+      const value = JSON.stringify(state)
+      this.storage.setItem(this.key, value)
+      this.storage.setItem(this.integrityKey, integrityOf(value))
     } catch (e) {
       console.warn(e)
     }
   }
+}
+
+function integrityOf(str: string): string {
+  return (2147483648 + hashCodeOf(str)).toString(36)
+}
+
+// https://stackoverflow.com/a/8076436
+function hashCodeOf(str: string): number {
+  let hash = 0
+  for (let i = 0; i < str.length; i++) {
+    const c = str.charCodeAt(i)
+
+    hash = (hash << 5) - hash + c
+    hash = hash & hash // Convert to 32bit integer
+  }
+
+  return hash
 }
