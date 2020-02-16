601b91ec62cf7cb31675fa392db2b572fbca3b66
kazuma1989 <funifuni.1204@gmail.com>
2020-02-16 18:10:35 +0900

    feat: タイマーが動作するように
    残り時間なくなってもとくに何も起きない

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,7 +1,32 @@
-import css from '/app/web_modules/csz.js'
-import { useEffect } from '/app/web_modules/preact/hooks.js'
-import { useDispatch, useStore } from './reducer.js'
+import { useEffect, useState } from '/app/web_modules/preact/hooks.js'
+import { useSelector } from './reducer.js'
 
 export default function Timer({ className }: { className?: string }) {
-  return <div className={className}>5:00</div>
+  const gameStartAt = useSelector(state => state.gameStartAt)
+
+  const [currentTime, setCurrentTime] = useState(Date.now())
+  useEffect(() => {
+    const timerId = setInterval(() => setCurrentTime(Date.now()), 1000)
+
+    return () => clearInterval(timerId)
+  }, [])
+
+  const timeLeft = 300 * 1000 - (currentTime - gameStartAt)
+
+  return <div className={className}>{toMMSS(timeLeft)}</div>
+}
+
+function toMMSS(duration: number) {
+  if (duration < 0) {
+    return '0:00'
+  }
+
+  const msec = duration % 1000
+  const sec = ((duration - msec) % 60000) / 1000
+  const min = (duration - sec * 1000 - msec) / 60000
+
+  const SS = sec.toString().padStart(2, '0')
+  const MM = min.toString()
+
+  return `${MM}:${SS}`
 }
